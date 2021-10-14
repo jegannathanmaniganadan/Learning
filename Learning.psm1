4e2c4f6b8f78121beda004b009ae0f3b0da2cd45
Manigandan Jegannathan <32796875+jegannathanmaniganadan@users.noreply.github.com>
2021-10-14 13:54:56 +0530

    samples

--- Learning.psm1
+++ Learning.psm1
@@ -1,1 +1,32 @@
 
+function Show-WelcomeMessage {
+    <#
+        .SYNOPSIS
+            Shows a welcome message for the given village
+
+        .DESCRIPTION
+            Shows a welcome message for the given village
+
+        .PARAMETER VillageName
+            Name of the village
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]
+        $VillageName
+    )
+
+    Write-Host ("==============================")
+
+    Write-Host ("Welcome to {0}" -f $VillageName)
+
+    Write-Host ("==============================")
+}
+
+function Show-CiCdWelcome {
+    return [PSCustomObject]@{
+        Name = "CICD"
+        WelcomeMessage = "Welcome to CICD"
+    }
+}
